7820d23c5f5bd6f73d6cb4a883c9cd8a90198b96
developer <108555356+site-page-test@users.noreply.github.com>
2022-12-31 21:55:52 +0530

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -72,7 +72,7 @@ audioElement.addEventListener('waiting',function(){
 
 //Images event listners to play there specific audio
 document.getElementById('0').addEventListener('click',function(){
-    audioElement.src= "songs/_Jaikal_Mahakal_-_Lyric_(getmp3.pro).mp3";
+    audioElement.src= "/songs/_Jaikal_Mahakal_-_Lyric_(getmp3.pro).mp3";
     audioElement.currentTime=0;
     audioElement.play();
     masterPlay.removeAttribute('src');
